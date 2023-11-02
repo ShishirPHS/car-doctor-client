12d5ebfcda07bc7f95e92a68d0a41f883b220e7f
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-11-03 01:33:42 +0600

    created useServices custom hook

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -1,14 +1,16 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+import useServices from "../../../hooks/useServices";
 import ServiceCard from "./ServiceCard/ServiceCard";
 
 const Services = () => {
-  const [services, setServices] = useState([]);
+  // const [services, setServices] = useState([]);
 
-  useEffect(() => {
-    fetch("http://localhost:5000/services")
-      .then((res) => res.json())
-      .then((data) => setServices(data));
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:5000/services")
+  //     .then((res) => res.json())
+  //     .then((data) => setServices(data));
+  // }, []);
+  const services = useServices();
 
   return (
     <div className="container mx-auto my-56">
@@ -26,7 +28,7 @@ const Services = () => {
         </p>
       </div>
       <div className="mt-12 grid grid-cols-1 lg:grid-cols-3 gap-6">
-        {services.map((service) => (
+        {services?.map((service) => (
           <ServiceCard key={service._id} service={service}></ServiceCard>
         ))}
       </div>
